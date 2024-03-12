cd7f9f2af1f81f036aadafb9f3c5645eb12d2113
S-Romeaux <sal.4845@gmail.com>
2024-03-11 19:23:25 -0700

    local version functional

--- server/server.js
+++ server/server.js
@@ -17,10 +17,8 @@ async function connectToMongoDB() {
       useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB!!!');
-    
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
-   
   }
 }
 
